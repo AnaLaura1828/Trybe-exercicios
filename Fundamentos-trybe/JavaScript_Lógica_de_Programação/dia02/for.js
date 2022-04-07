21163622a73233b407a7b73b53bc161bb9e8a4c0
Ana Laura <aninhamelo.8santos@gmail.com>
2022-04-07 16:05:13 -0300

    exercicios de fixação

---
+++ Fundamentos-trybe/JavaScript_Lógica_de_Programação/dia02/for.js
@@ -0,0 +1,13 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(var index = 0;index < groceryList.length; index +=1) {
+    console.log(groceryList);
+}
+
+
+
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (var sunny of names) {
+    console.log(sunny);
+}
