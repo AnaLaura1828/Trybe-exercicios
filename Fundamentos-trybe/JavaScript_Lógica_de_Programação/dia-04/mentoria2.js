69d3036fad540e7fd5b78f0292a925ca8fecda1b
Ana Laura <aninhamelo.8santos@gmail.com>
2022-04-11 21:47:21 -0300

    modificando exercicio

---
+++ Fundamentos-trybe/JavaScript_Lógica_de_Programação/dia-04/mentoria2.js
@@ -0,0 +1,19 @@
+let array = [5, 3, 2, 20, 40];
+
+function imparesPares(valores) {
+    let resultado = {
+        pares: 0,
+        impares: 0,
+    }
+    for(let index = 0; index < valores.length; index +=1) {
+        if(valores[index]% 2 === 0) {
+            resultado.pares +=1;
+        }else {
+            resultado.impares +=1;
+        }
+    }
+    return resultado
+}
+console.log(pares, impares);
+    
+
