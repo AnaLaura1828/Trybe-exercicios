2b2d10be49cd7c7fd0233122ed767dee542fe991
Ana Laura <aninhamelo.8santos@gmail.com>
2022-05-10 10:00:45 -0300

    exercicio do dia

---
+++ Fundamentos-trybe/JavaScript-ES6/dia-02/fixação.js
@@ -0,0 +1,87 @@
+const student1 = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkills: 'Ótimo',
+  };
+  
+  const student2 = {
+    Html: 'Bom',
+    Css: 'Ótimo',
+    JavaScript: 'Ruim',
+    SoftSkills: 'Ótimo',
+    Git: 'Bom', // chave adicionada
+  };
+
+  const listSkills = (student) => {
+    const arrayOfSkills = Object.keys(student);
+    for (const index in arrayOfSkills) {
+      console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+    }
+  };
+  
+  console.log('Estudante 1');
+  listSkills(student1);
+  
+  console.log('Estudante 2');
+  listSkills(student2);
+
+  //segundo
+  const coolestTvShow = {
+    name: "BoJack Horseman",
+    genre: "adult animation",
+    createdBy: "Raphael Bob-Waksberg",
+    favoriteCharacter: "Princess Carolyn",
+    quote: "Princess Carolyn always lands on her feet.",
+    seasons: 6,
+  };
+  
+  // for (const property in coolestTvShow) {
+  //   console.log(coolestTvShow[property]);
+  // }
+  
+  console.log(Object.values(coolestTvShow));
+
+  var obj = { foo: 'bar', baz: 42 };
+
+console.log(obj); // Map { foo: "bar", baz: 42 }
+
+
+const países = {
+    França: 'Paris',
+    Brasil: 'Brasília',
+    Espanha: 'Madrid',
+    Portugal: 'Lisboa',
+  };
+  const pairKeyValue = Object.entries(países);
+  console.log(pairKeyValue);
+  for(index in pairKeyValue) {
+    console.log('--------');
+    console.log('País:', pairKeyValue[index][0]);
+    console.log('Capital:', pairKeyValue[index][1]);
+  };
+
+  const person = {
+    name: 'Roberto',
+  };
+  
+  const lastName = {
+    lastName: 'Silva',
+  };
+  
+  const clone = Object.assign(person, lastName);
+  
+  console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+  console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+  clone.name = 'Maria';
+
+console.log('Mudando a propriedade name através do objeto clone')
+console.log(clone); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log('--------------');
+
+person.lastName = 'Ferreira';
+
+console.log('Mudando a propriedade lastName através do objeto person');
+console.log(clone); // Output: { name: 'Maria', lastName: 'Ferreira' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Ferreira' }
