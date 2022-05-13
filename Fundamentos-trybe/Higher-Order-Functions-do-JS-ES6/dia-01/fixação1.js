4d3c2e0030e17142e502d706ce3e8b00b9b1e1d1
Ana Laura <aninhamelo.8santos@gmail.com>
2022-05-13 20:32:08 -0300

    exercicio do dia

---
+++ Fundamentos-trybe/Higher-Order-Functions-do-JS-ES6/dia-01/fixação1.js
@@ -0,0 +1,15 @@
+const wakeUp = () => 'acordando!!'
+// console.log(wakeUp());
+
+const drinkCoffe = () => 'Bora tomar café!!'
+// console.log(drinkCoffe());
+
+const goSleep = () => 'Partiu dormir'
+// console.log(goSleep());
+
+const doingThings = (callback) => {
+    return callback
+}
+console.log(doingThings(wakeUp()));
+
+
