b1b856a13b82d15313c2ca64d6cbd8b55b4a9a54
Ana Laura <aninhamelo.8santos@gmail.com>
2022-05-31 20:24:27 -0300

    nova pasta

---
+++ Front-End/JavaScript-Testes-Assíncronos/dia-01/test.js
@@ -0,0 +1,90 @@
+// cicles.test.js
+
+let cities = [];
+
+const addCity = (city) => {
+  cities.push(city);
+};
+
+const removeCity = (city) => {
+  cities = cities.filter((eachCity) => eachCity !== city);
+};
+
+// cicles.test.js
+
+// let cities = [];
+
+// const addCity = (city) => {
+//  cities.push(city);
+// };
+
+// const removeCity = (city) => {
+//  cities = cities.filter((eachCity) => eachCity !== city);
+// };
+
+test('Testa a função addCity', () => {
+  expect.assertions(4);
+  addCity('Campinas');
+  addCity('Goiania');
+  addCity('Recife');
+  expect(cities).toHaveLength(3);
+  expect(cities).toContain('Campinas');
+  expect(cities).toContain('Goiania');
+  expect(cities).toContain('Recife');
+});
+
+test('Testa a função removeCity', () => {
+  expect.assertions(4);
+  removeCity('Campinas');
+  expect(cities).toHaveLength(2);
+  expect(cities).not.toContain('Campinas');
+  expect(cities).toContain('Goiania');
+  expect(cities).toContain('Recife');
+});
+
+//exercicio 7
+const uppercase = (str, callback) => {
+  setTimeout(() => {
+    callback(str.toUpperCase());
+  }, 500);
+};
+
+it('Teste para ver se "todas" as letras são "MAIUSCULAS"', (done) => {
+    uppercase('todas', (str) => {
+        try {
+            expect(str).toBe('TODAS');
+            done();
+        } catch (error) {
+            done(error);
+        }
+    });
+});
+
+//exercicio 9
+// Verifique se a importação do arquivo correto está sendo feita.
+
+const { getPokemonDetails } = require("./get-pokemon-details");
+
+describe("A função getPokemonDetails", () => {
+  it("retorna erro quando procuramos um pokemon que não existe no banco de dados", (done) => {
+    const expectedError = new Error('Não temos esse pokémon para você :(');
+
+    function callback(error, result) {
+      expect(error).toEqual(expectedError);
+      done();
+    }
+
+    getPokemonDetails((pokemon) => pokemon.name === 'Pikachu', callback);
+  });
+
+  it("retorna um pokemon que existe no banco de dados", (done) => {
+    const expectedString = 'Olá, seu pokémon é o Charmander, o tipo dele é Fire e a habilidade principal dele é Ember';
+
+    function callback(err, result) {
+      expect(result).toBe(expectedString);
+      done();
+    }
+
+    getPokemonDetails((pokemon) => pokemon.name === 'Charmander', callback);
+  });
+});
